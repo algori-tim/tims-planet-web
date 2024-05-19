d141bb83b04a7340f07093ff34b22716b3dd2349
Tim Johnson <timaj88@gmail.com>
2024-05-19 12:24:54 -0500

    style clean up

--- src/Components/AboutMe/Aboutme.tsx
+++ src/Components/AboutMe/Aboutme.tsx
@@ -25,15 +25,16 @@ const AboutMe = () => {
       <h4>The past...</h4>
       <p>
         Before joining Subvrsive, I spent some time at Tech9, where I had my first foreys into developing full-stack
-        solutions. I crafted a custom C#.NET/Typescript/React cloud application for streamlining QA processes and
-        boosting developer productivity. My role involved juggling various technologies and always staying ahead of the
-        curve to deliver reliable and efficient applications with a focus on high quality and extensive test coverage.
+        solutions. While there, I crafted a custom C#.NET/Typescript/React cloud application for streamlining QA
+        processes and boosting developer productivity. My role involved learning a variety of new technologies and
+        staying ahead of the curve to deliver reliable and efficient applications with a focus on high quality and
+        extensive test coverage.
       </p>
       <p>
         Prior to that, I cut my teeth at American National, starting as an eager intern and quickly rising to lead
-        roles. I had my hands on developing pivotal C# ASP.NET APIs that became central to the company's operations. My
-        journey there included implementing robust content management solutions and driving best practices in unit
-        testing and dependency injection. Leading and mentoring teams, I guided architectural decisions and ensured
+        roles. While there I helped develop pivotal C# ASP.NET APIs that became central to the company's operations. My
+        journey there included implementing custom content management solutions and driving best practices in unit
+        testing and API design. Later, while leading and mentoring teams, I guided architectural decisions and ensured
         seamless project delivery. It was here that I truly honed my skills in building scalable, secure, and
         high-performing web applications while fostering a collaborative and innovative work environment.
       </p>
